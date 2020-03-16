a6157e987473b229eabcc86e9fdee4c1a2fce89a
勤硕 <qinshuo.lqs@alibaba-inc.com>
2020-03-16 11:34:08 +0800

    fix: history events sort

--- client/index/src/components/History/index.js
+++ client/index/src/components/History/index.js
@@ -7,17 +7,19 @@ const History = ({ events }) => {
   // red | gray | green
   return (
     <Timeline reverse mode="left">
-      {events.map((event, index) => {
-        return (
-          <Timeline.Item key={index} color="green" position="left">
-            <span>{moment(event.time).format('YYYY-MM-DD HH:mm:ss')} </span>
-            <Status state={event.event_name} />
-            <strong>{event.name} </strong>
-            <span> ({event.id}) </span>
-            <span> - {event.destination}</span>
-          </Timeline.Item>
-        );
-      })}
+      {events
+        .sort((a, b) => a.time - b.time)
+        .map((event, index) => {
+          return (
+            <Timeline.Item key={index} color="green" position="left">
+              <span>{moment(event.time).format('YYYY-MM-DD HH:mm:ss')} </span>
+              <Status state={event.event_name} />
+              <strong>{event.name} </strong>
+              <span> ({event.id}) </span>
+              <span> - {event.destination}</span>
+            </Timeline.Item>
+          );
+        })}
     </Timeline>
   );
 };
